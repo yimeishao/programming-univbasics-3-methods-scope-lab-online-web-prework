045d39e8da3c8e0c49391850e9b54a456dbf5b35
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-03 19:45:52 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,6 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+ - status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+ + status = 'It's-a me, Mario!'
   puts phrase
 end
 
