f1ce7f58dff6e892005e57a79fc8c207e5aee05b
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-03 19:50:41 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -5,7 +5,7 @@ def mario
 end
 
 def toadstool
-  + status = 'Thank You Maria! But Our Princess Is In Another Castle!'
+  + status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
